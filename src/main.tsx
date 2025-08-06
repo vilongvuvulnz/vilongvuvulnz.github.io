e680946b4759a3b909c87f4accf29a46aecae3da
kiuyha <ketutshridhara@gmail.com>
2025-08-06 17:26:48 +0800

    optimize

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { DataProvider } from "./contexts/DataContext.tsx";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import { ErrorBoundary } from "react-error-boundary";
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,51 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		{
+			name: "vite-plugin-non-blocking-css",
+			apply: "build", // Apply this plugin only during the build process
+			enforce: "post", // Make sure it runs after CSS is generated
+			transformIndexHtml(html) {
+				const regex =
+					/<link rel="stylesheet"(.*?)href="(.+?\.css)"(.*?)>/;
+				const match = html.match(regex);
+
+				if (match) {
+					const cssUrl = match[2];
+					const preloadLink = `
+					<link rel="preload" crossorigin href="${cssUrl}" as="style">
+					<link rel="stylesheet" crossorigin href="${cssUrl}">
+					`;
+					// Replace the original link tag with the new preload version
+					return html.replace(match[0], preloadLink);
+				}
+
+				return html;
+			},
+		},
+	],
 	server: {
 		allowedHosts: true,
-	}
+	},
+	build: {
+		minify: "terser", // Use Terser for minification since give more smaller bundle
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					// Group React related packages into a 'react-vendor' chunk
+					if (id.includes("react")) {
+						return "react-vendor";
+					}
+
+					// Group all other node_modules into a general 'vendor' chunk
+					if (id.includes("node_modules")) {
+						return "vendor";
+					}
+				},
+			},
+		},
+	},
 });
